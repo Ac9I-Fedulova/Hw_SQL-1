2da9b5b417bb7b6bf6e63a83f8cafab9364dfaac
Anastasia Fedulova <fedulova.a.m@bk.ru>
2025-04-07 18:13:44 +0300

    added tests

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,33 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class LoginPage {
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+    private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
+
+    public void verifyErrorNotificationVisiblity(String exactedText) {
+        errorNotification.shouldBe(visible).shouldHave(text(exactedText));
+    }
+
+    public void checkButtonUnavailability() {
+        boolean isEnabled = loginButton.isEnabled();
+        assertFalse(isEnabled);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+}
---
+++ src/test/java/ru/netology/test/BankLoginPersonalAccountTest.java
@@ -0,0 +1,70 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class BankLoginPersonalAccountTest {
+    LoginPage loginPage;
+    DataHelper.AuthInfo authInfo = DataHelper.getAuthInfo();
+
+    @AfterAll
+    static void teardownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @AfterEach
+    void teardown() {
+        cleanAuthCodes();
+    }
+
+    @Test
+    @DisplayName("Должны успешно войти в личный кабинет")
+    void shouldLoginSuccessfully() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Должны получить уведомление об ошибке при входе со случайным пользователем без добавления в базу")
+    void shouldGetErrorNotificationIfLoginInvalidUser() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisiblity("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Должны получить уведомление об ошибке при входе валидного пользователя с  невалидным кодом подтверждения")
+    void shouldGetErrorNotificationIfLoginValidUserAndRandomVerificationCode() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisiblity("Ошибка! Неверно указан код! Попробуйте ещё раз.");
+    }
+
+//    @Test
+//    @DisplayName("Должны получить уведомление о блокировке аккаунта пользователя после трёх попыток неверного ввода пароля")
+//    void shouldLockSystemAfterThreeAttemptsEnterInvalidPassword() {
+//        var authInfo = DataHelper.generateRandomUser();
+//        for (int i = 0; i < 3; i++) {
+//            loginPage.validLogin(authInfo);
+//        }
+//        assertAll(() -> loginPage.verifyErrorNotificationVisiblity("Аккаунт заблокирован после трех неудачных попыток входа," +
+//                        "Вам следует обратитесь в офис Банка"),
+//                () -> loginPage.checkButtonUnavailability());
+//    }
+}
